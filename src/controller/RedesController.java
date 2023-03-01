b18dc243162e3949ae054ad7faaa891e3e3ccfe8
knn654 <kaua.nascimento@fatec.sp.gov.br>
2023-03-01 10:43:57 -0300

    Portabilização pro Linux

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -15,31 +15,56 @@ public class RedesController {
 	public void ip(String processo) {
 		int contador = 0;
 		try {
-			Process p = Runtime.getRuntime().exec(processo);
-			InputStream fluxo = p.getInputStream();
-			InputStreamReader leitor = new InputStreamReader(fluxo);
-			BufferedReader buffer = new BufferedReader(leitor);
-			String linha = buffer.readLine();
-			while (linha != null) {
-				if (linha.contains("Sem nome")) {
-					contador = 1;
-				} else if (linha.contains("Ethernet")){
-					System.out.println(linha);
-				}
-				if (linha.contains("IPv4") && contador == 0) {
-					System.out.println(linha);
-				} 
+			if (os().contains("Windows")) {
+				Process p = Runtime.getRuntime().exec(processo);
+				InputStream fluxo = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				while (linha != null) {
+					if (linha.contains("Sem nome")) {
+						contador = 1;
+					} else if (linha.contains("Ethernet")){
+						System.out.println(linha);
+					}
+					if (linha.contains("IPv4") && contador == 0) {
+						System.out.println(linha);
+					} 
 					linha = buffer.readLine();
-			}
+				}
 
-			buffer.close();
-			leitor.close();
-			fluxo.close();
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+			} else {
+				if (os().contains("Linux")) {
+					Process p = Runtime.getRuntime().exec(processo);
+					InputStream fluxo = p.getInputStream();
+					InputStreamReader leitor = new InputStreamReader(fluxo);
+					BufferedReader buffer = new BufferedReader(leitor);
+					String linha = buffer.readLine();
+					while (linha != null) {
+						if (linha.contains("Unnamed")) {
+							contador = 1;
+						} else if (linha.contains("Ethernet")){
+							System.out.println(linha);
+						}
+						if (linha.contains("IPv4") && contador == 0) {
+							System.out.println(linha);
+						} 
+						linha = buffer.readLine();
+					}
+
+					buffer.close();
+					leitor.close();
+					fluxo.close();
+				}
+			}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
 			
-		} catch (Exception e) {
-			e.printStackTrace();
-			
-		}
+			}
 	}
 	public void ping(String processo) {
 		try {
--- src/view/Main.java
+++ src/view/Main.java
@@ -10,17 +10,26 @@ public class Main {
 		RedesController redescontroller = new RedesController();
 		String nomeSistema = redescontroller.os();
 		System.out.println("Você está usando " + nomeSistema);
+		String processo = "";
 		int opcao;
 		do {
 			opcao = Integer.parseInt(JOptionPane.showInputDialog(
 					"Digite a opção desejada:\n" + "1 - Método IP\n" + "2 - Método Ping\n" + "9 - Finalizar"));
 			switch (opcao) {
 			case 1:
-				String processo = "IPCONFIG";
+				if (nomeSistema.contains("Windows")) {
+					processo = "IPCONFIG";
+				} else {
+					processo = "IFCONFIG";
+				}
 				redescontroller.ip(processo);
 				break;
-			case 2:
-				processo = "PING -4 -n 10 www.google.com.br";
+			case 2: 
+				if (nomeSistema.contains("Windows")) {
+					processo = "PING -4 -n 10 www.google.com.br";
+				} else {
+					processo = "PING -4 c 10 www.google.com.br";
+				}
 				redescontroller.ping(processo);
 				break;
 			case 9:
